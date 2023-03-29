9c23c6318da93152410efa93944ebf87327b11d8
JohnOkari <johnnokari@gmail.com>
2023-03-29 13:54:22 +0300

    add update meal

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -14,9 +14,29 @@ class MealsController < ApplicationController
         end
     end
 
+    def update
+        meal = (find_meal)
+        if meal
+            if meal.update(meal_params)
+                render json: meal
+                
+            else
+                render json: { errors: meal.errors.full_messages }, status: :unprocessable_entity
+            end
+        else
+            render json: { error: "Meal not found" }, status: :not_found
+        end
+    end
+
+    
+
     private
 
     def find_meal
         Meal.find_by(id: params[:id])
+    end
+
+    def meal_params
+        params.permit(:food, :meal_type, :calories)
     end
 end
