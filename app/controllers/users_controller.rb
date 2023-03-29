5dbbf8d38dc9acff06bb373ef66e58fe7c3f5ce5
JohnOkari <johnnokari@gmail.com>
2023-03-29 19:13:56 +0300

    add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,22 @@ class UsersController < ApplicationController
         render json: @current_user
     end
 
-    
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
 
 
   private
+
+  def user_params
+    params.permit(:username, :password, :password_cornfirmation)
+  end
 
   def authorize
     @current_user = User.find_by(id: session[:user_id])
