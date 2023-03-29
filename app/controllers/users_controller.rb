4932a491cab798f1292d2a84418f412790f5fb1a
JohnOkari <johnnokari@gmail.com>
2023-03-29 19:09:30 +0300

    add current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    before_action :authorize
+
+    def show
+        render json: @current_user
+    end
+
+    
+
+
+  private
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  
+
 end
